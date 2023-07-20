4d26b6a4714a6185c74553693e4b3a682baf386a
Farhad Mohammad <25428408+Tachyon97@users.noreply.github.com>
2023-07-20 09:20:09 +0200

    reqs update for tenacity, EOF SSL error fix

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
 import time
+import ssl
 from datetime import datetime, timedelta
+from tenacity import retry, stop_after_attempt, wait_exponential
 
 from config import load_config
 from feed import check_feed, load_seen_links, save_seen_links
@@ -11,7 +13,7 @@ config = load_config()
 
 uploader = WordPressUploader()
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 seen_links = load_seen_links()
@@ -20,6 +22,15 @@ start_time = datetime.now()
 
 # Specify the categories here as a list
 categories = ["News", "World News"]
+
+@retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=4, max=10))
+def upload_with_retry(title, content, image):
+    try:
+        uploader.upload(title, content, image, categories=categories)
+        logger.debug("Uploaded post: %s", title)
+    except ssl.SSLError as e:
+        logger.error("SSL Error: %s", e)
+        raise  # This will cause the upload to be retried
 
 while True:
     logger.info("Checking feed for new articles...")
@@ -38,8 +49,7 @@ while True:
         logger.debug("Generated image: %s", image)
 
         logger.info("Uploading post to WordPress...")
-        uploader.upload(title, content, image, categories=categories)
-        logger.debug("Uploaded post: %s", title)
+        upload_with_retry(title, content, image)
         
         num_articles += 1
 
@@ -47,6 +57,7 @@ while True:
     next_check_time = datetime.now() + timedelta(seconds=config['loop_delay'])
     logger.info("Next time to check for updates: %s", next_check_time.strftime("%H:%M"))
     logger.info("Generated articles so far: %d", num_articles)
+    logger.info("Total articles processed: %d", len(seen_links))
     logger.info("Running for: %s", str(elapsed_time))
 
     # Save seen links to the JSON file
